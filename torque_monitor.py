4dbc26be34912e495317b50734653412c3ea8011
Elison-Maiko-Oliveira <elison_maiko_oliveira_act@whirlpool.com>
2026-07-05 20:55:59 -0300

    last update torque_monitor

--- torque_monitor.py
+++ torque_monitor.py
@@ -23,31 +23,31 @@ base_A = math.radians(131.08)
 base_B = math.radians(70.0) # Rotaciona a base para colocar a peça em outro lugar
 
 # Ponto A (Lado de Pegar)
-q_alto_A  = [base_A, math.radians(-90), math.radians(90), math.radians(-90), math.radians(-90), 0]
-q_baixo_A = [base_A, math.radians(-60), math.radians(60), math.radians(-90), math.radians(-90), 0]
+pos_alto_A  = [base_A, math.radians(-90), math.radians(90), math.radians(-90), math.radians(-90), 0]
+pos_baixo_A = [base_A, math.radians(-60), math.radians(60), math.radians(-90), math.radians(-90), 0]
 
 # Ponto B (Lado de Soltar)
-q_alto_B  = [base_B, math.radians(-90), math.radians(90), math.radians(-90), math.radians(-90), 0]
-q_baixo_B = [base_B, math.radians(-60), math.radians(60), math.radians(-90), math.radians(-90), 0]
+pos_alto_B  = [base_B, math.radians(-90), math.radians(90), math.radians(-90), math.radians(-90), 0]
+pos_baixo_B = [base_B, math.radians(-60), math.radians(60), math.radians(-90), math.radians(-90), 0]
 
 # 4. Função Mágica: Move e calcula torque em tempo real
-def mover_e_monitorar(q_alvo, F_externa, nome_movimento):
+def mover_e_monitorar(pos_alvo, F_externa, nome_movimento):
     print(f"\n--- {nome_movimento} ---")
     
     # Inicia o movimento sem travar o código
-    rtde_c.moveJ(q_alvo, speed=0.5, acceleration=0.5, asynchronous=True)
+    rtde_c.moveJ(pos_alvo, speed=0.5, acceleration=0.5, asynchronous=True)
     
     while True:
-        q_atual = rtde_r.getActualQ()
+        pos_atual = rtde_r.getActualQ()
         
         # Verifica se chegou
-        distancia = sum(abs(atual - alvo) for atual, alvo in zip(q_atual, q_alvo))
+        distancia = sum(abs(atual - alvo) for atual, alvo in zip(pos_atual, pos_alvo))
         if distancia < 0.05:
             print(f"\n[✓] Destino alcançado!")
             break
             
         # Calcula o Jacobiano Transposto e o Torque
-        J = ur10.jacob0(q_atual)
+        J = ur10.jacob0(pos_atual)
         torques = J.T @ F_externa
         
         # Formata a string para o terminal e imprime na mesma linha
@@ -61,25 +61,25 @@ try:
     print("\nIniciando Ciclo Pick and Place...")
     
     # ETAPA 1: Vai buscar (Vazio)
-    mover_e_monitorar(q_alto_A, F_vazia, "1. Indo para Posição de Espera (Vazio)")
-    mover_e_monitorar(q_baixo_A, F_vazia, "2. Abaixando para pegar peça (Vazio)")
+    mover_e_monitorar(pos_alto_A, F_vazia, "1. Indo para Posição de Espera (Vazio)")
+    mover_e_monitorar(pos_baixo_A, F_vazia, "2. Abaixando para pegar peça (Vazio)")
     
     # SIMULAÇÃO DE PEGAR A PEÇA (Pausa rápida)
     print("\n>>> PEGANDO A PEÇA (10 KG) <<<")
     time.sleep(1)
     
     # ETAPA 2: Transporta (Cheio)
-    mover_e_monitorar(q_alto_A, F_carga, "3. Levantando a peça (Carga Máxima!)")
-    mover_e_monitorar(q_alto_B, F_carga, "4. Movendo para o outro lado (Carga Máxima!)")
-    mover_e_monitorar(q_baixo_B, F_carga, "5. Abaixando para soltar (Carga Máxima!)")
+    mover_e_monitorar(pos_alto_A, F_carga, "3. Levantando a peça (Carga Máxima!)")
+    mover_e_monitorar(pos_alto_B, F_carga, "4. Movendo para o outro lado (Carga Máxima!)")
+    mover_e_monitorar(pos_baixo_B, F_carga, "5. Abaixando para soltar (Carga Máxima!)")
     
     # SIMULAÇÃO DE SOLTAR A PEÇA
     print("\n>>> SOLTANDO A PEÇA <<<")
     time.sleep(1)
     
     # ETAPA 3: Retorna (Vazio)
-    mover_e_monitorar(q_alto_B, F_vazia, "6. Levantando garra (Vazio)")
-    mover_e_monitorar(q_alto_A, F_vazia, "7. Retornando ao Início (Vazio)")
+    mover_e_monitorar(pos_alto_B, F_vazia, "6. Levantando garra (Vazio)")
+    mover_e_monitorar(pos_alto_A, F_vazia, "7. Retornando ao Início (Vazio)")
     
     print("\nCiclo finalizado com sucesso!")
 
